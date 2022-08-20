76adb4d6b42ce48785f2e76a772e36e2e1fdf7fa
surya <cpgnsc@gmail.com>
2022-08-20 10:15:21 +0545

    component files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List";
+import Heading from "./Heading";
+
+function App(){
+    return(
+        <>
+<Heading/>
+<List/>
+</>
+    )
+}
+
+export default App;
---
+++ src/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function List(){
+    return(
+        <>        <h2>The example  List item is </h2>
+    <ol>
+        <li>ram</li>
+        <li>shyam</li>
+        <li>geeta</li>
+        <li>hari</li>
+        <li>sita</li>
+
+     </ol>
+     </>
+
+    )
+}
+export default List;
