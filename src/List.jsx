c60da05e5bd9ce357695b341ba8d344c9bf19ccf
surya <cpgnsc@gmail.com>
2022-08-20 14:32:46 +0545

    import export module

--- src/List.jsx
+++ src/List.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import MyName, {MyAdd,MyAge,MyColl} from "./content";
 function List(){
     return(
-        <>        <h2>The example  List item is </h2>
+        <>        
+        <h2>The example  List item is </h2>
     <ol>
-        <li>ram</li>
-        <li>shyam</li>
-        <li>geeta</li>
-        <li>hari</li>
-        <li>sita</li>
+        <li> {MyName} </li>
+        <li>{MyAdd}</li>
+        <li>{MyAge}</li>
+        <li>{MyColl}</li>
+        
+       
 
      </ol>
      </>
---
+++ src/content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyName ="xyz chapai";
+
+const MyAdd ="Nawalpur";
+
+const MyAge ="21";
+
+const MyColl ="BMC";
+
+export default MyName;
+export {MyAdd,MyAge, MyColl }; 
